0f3070f35278a3cc78d9c5503c53bd3704efab10
anadiangelsingh <46760095+anadiangelsingh@users.noreply.github.com>
2019-01-17 10:09:34 +0530

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>My Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+        <div class="logo"> </div>
+		<div class="nav"> 
+            <h1 class="titl"> Welcome to my Page. </h1>
+            <h3 class="desc"> This webpage is all about me. What I've Learned and Who am I? </h3>
+        </div>
+		<div class="side"> 
+            <a href="about.php" class="link"> About Me </a>
+            <br><br>
+            <a href="myDesigns.php" class="link"> My Designs </a>
+            <br><br>
+            <a href="photos.php" class="link"> My Life at a Glance </a>
+            <br><br>
+            <a href="contact.php" class="link"> Contact Me </a>
+            <br><br>
+            <a href="locate.php" class="link"> Find Me </a>
+            <br><br>
+
+        </div>
+		<div class="main"> 
+            <h1 class="mainHead"> This is my First Webpage</h1>
+            <p class="para"> I am a MCA Student from Chandigarh University. I love to code. I Love solving Problems. Please Refer Links to know more about me...;-D </p>
+
+        </div>
+		<div class="foot"> </div>
+</body>
+</html>
